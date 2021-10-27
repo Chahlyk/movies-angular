288b10a7529c8e4a54a88d4ceeddc19b669ea068
Chahlyk <Lubimiekonfeti312@gmail.com>
2021-10-27 17:15:48 +0300

    add favourites

--- src/app/detail-poster/detail-poster.component.ts
+++ src/app/detail-poster/detail-poster.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PosterService } from "../poster.service";
 import { IPoster } from "../shared/interfaces";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Location } from '@angular/common';
 import { CartService } from "../cart.service";
 
@@ -13,28 +13,44 @@ import { CartService } from "../cart.service";
 export class DetailPosterComponent implements OnInit {
   public posters: IPoster[] = []
   public poster!: IPoster
+  public nextId!: IPoster
   public linkForImage: string = 'https://image.tmdb.org/t/p/w342';
   public loading: boolean = false;
   public required: boolean = false;
+  public num!: number
 
   constructor(
     private route: ActivatedRoute,
     private posterService: PosterService,
     private location: Location,
     private cartService: CartService,
+    private router: Router
   ) { }
 
   public ngOnInit(): void {
     this.getPoster();
   }
 
+  public getData() {
+    this.posterService.getData()
+      .subscribe(posters => {
+        this.posters = posters.results
+        this.num = this.posters.findIndex(film => film.id === this.poster.id)
+        this.nextId = this.posters[this.num + 1]
+        console.log(this.nextId.id)
+      })
+  }
+
   public getPoster(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.posterService.getPoster(id)
-      .subscribe(poster => {
-        this.poster = poster;
-        this.loading = true;
-      });
+    this.route.params.subscribe(params => {
+      this.posterService.getPoster(params.id)
+        .subscribe(poster => {
+          this.poster = poster;
+          this.loading = true;
+          this.getData();
+        });
+    });
+
   }
 
   public goBack(): void {
@@ -45,5 +61,9 @@ export class DetailPosterComponent implements OnInit {
     this.cartService.addToFavourite(film)
     this.required = true
     console.log('finish')
+  }
+
+  nextMovie() {
+    this.router.navigate(['/details', this.nextId.id])
   }
 }
