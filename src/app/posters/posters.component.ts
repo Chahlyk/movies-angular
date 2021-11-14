3fd60143e98dbb00d40ad1404390636aaa18174a
Chahlyk <lubimiekonfeti312@gmail.com>
2021-11-14 22:39:56 +0200

    add pagination edit k

--- src/app/posters/posters.component.ts
+++ src/app/posters/posters.component.ts
@@ -11,8 +11,6 @@ export class PostersComponent implements OnInit {
   public posters: IPoster[] = [];
   public page!: number;
   public totalItems!: number;
-  // private totalPages!: number;
-  // public  data!: any;
 
   public constructor(private posterService: PosterService) {}
 
@@ -20,67 +18,12 @@ export class PostersComponent implements OnInit {
     this.getData(1);
   }
 
-  // public first(): void {
-  //   this.posterService.getData(1)
-  //     .subscribe((posters: IData) => {
-  //       this.posters = posters.results;
-  //       this.page = posters.page;
-  //     })
-  // }
-  //
-  // public prev(): void {
-  //   this.posterService.getData(this.page - 1)
-  //     .subscribe((posters: IData) => {
-  //       this.posters = posters.results;
-  //       this.page = posters.page;
-  //     })
-  // }
-  //
-  // public innerFirst(): void {
-  //   this.posterService.getData(this.page)
-  //     .subscribe((posters: IData) => {
-  //       this.posters = posters.results;
-  //       this.page = posters.page;
-  //     })
-  // }
-  //
-  // public innerSecond(): void {
-  //   this.posterService.getData(this.page + 1)
-  //     .subscribe((posters: IData) => {
-  //       this.posters = posters.results;
-  //     })
-  // }
-  //
-  // public innerThird(): void {
-  //   this.posterService.getData(this.page + 2)
-  //     .subscribe((posters: IData) => {
-  //       this.posters = posters.results;
-  //     })
-  // }
-  //
-  // public next(): void {
-  //   this.posterService.getData(this.page + 1)
-  //     .subscribe((posters: IData) => {
-  //       this.posters = posters.results;
-  //       this.page = posters.page;
-  //     })
-  // }
-  //
-  // public last(): void {
-  //   this.posterService.getData(this.totalPages)
-  //     .subscribe((posters: IData) => {
-  //       this.posters = posters.results;
-  //       this.page = posters.page;
-  //     })
-  // }
-
   private getData(page: number): void {
     this.posterService.getData(this.page)
       .subscribe((posters: IData) => {
         this.posters = posters.results;
         this.page = posters.page;
         this.totalItems = posters.total_results;
-        // this.totalPages = posters.total_pages
       })
   }
 }
